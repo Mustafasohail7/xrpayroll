0924310faeb289633da835bf08b5898c52439441
Mustafasohail7 <mustufasohail7@gmail.com>
2023-11-07 23:20:36 +0500

    fixed department-wise payments

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -12,13 +12,14 @@ function EmployeeList({ employeeDataArray,wallet,setWallet,updateBalance}) {
   }, {});
 
   const handleEmployeePay = async (destination) => {
+    console.log(destination)
     const client = new xrpl.Client("wss://s.altnet.rippletest.net:51233")
     await client.connect()
     const company_wallet = xrpl.Wallet.fromSeed(wallet.secret)
     const prepared = await client.autofill({
       "TransactionType": "Payment",
       "Account": wallet.address,
-      "Amount": "1000000",
+      "Amount": "10000000",
       "Destination": destination.toString(),
     })
     const signed = company_wallet.sign(prepared)
@@ -27,8 +28,24 @@ function EmployeeList({ employeeDataArray,wallet,setWallet,updateBalance}) {
     await client.disconnect()
   }
 
-  const handleDepartmentPay = async () => {
+  const handleDepartmentPay = async (department) => {
     const client = new xrpl.Client("wss://s.altnet.rippletest.net:51233")
+    await client.connect()
+    const company_wallet = xrpl.Wallet.fromSeed(wallet.secret)
+    console.log('paying department',department)
+    for(var i=0;i<employeesByDepartment[department].length;i++){
+      const prepared = await client.autofill({
+        "TransactionType": "Payment",
+        "Account": wallet.address,
+        "Amount": "10000000",
+        "Destination": employeesByDepartment[department][i].xrpWalletAddress.toString(),
+      })
+      const signed = company_wallet.sign(prepared)
+      const tx = await client.submitAndWait(signed.tx_blob)
+      updateBalance()
+    }
+    console.log('done')
+    await client.disconnect()
   }
 
   return (
@@ -38,14 +55,14 @@ function EmployeeList({ employeeDataArray,wallet,setWallet,updateBalance}) {
         <div key={department} className='employeeList'>
           <div className='department-container'>
             <h3>{department}</h3>
-            <button onClick={handleDepartmentPay}>pay</button>
+            <button onClick={()=>handleDepartmentPay(department)}>pay</button>
           </div>
           <ul>
             {employeesByDepartment[department].map((employee, index) => {
                 {/* console.log(employee) */}
                 return (
               <li key={index}><p>{employee.fullName}</p><p>({employee.baseSalary})</p>
-              <button onClick={handleEmployeePay}>pay</button></li>
+              <button onClick={()=>handleEmployeePay(employee.xrpWalletAddress)}>pay</button></li>
             )})}
           </ul>
         </div>
